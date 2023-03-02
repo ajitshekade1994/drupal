0df25370bb7e33c56caf073bd0d0797826afaf13
ajitshekade1994 <87651238+ajitshekade1994@users.noreply.github.com>
2023-03-02 16:33:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 /**
  * Root directory of Drupal installation.
  */
-print_r("Under Maintainance"); die; 
+
 define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
